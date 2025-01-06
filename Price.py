be5dd6e4a1756dd01a1b2a46c947016537c9428b
BAPPAYNE <sahilkatrodiyalwt@gmail.com>
2025-01-06 22:21:47 +0530

    Comment out stock price retrieval code and import yfinance

--- Price.py
+++ Price.py
@@ -1,12 +1,14 @@
-from bs4 import BeautifulSoup
-import requests
+# from bs4 import BeautifulSoup
+# import requests
 
-def stockPrice(query="Binance coin price"):
-    URL = "https://www.google.com/search?q=" + query
-    page = requests.get(URL).text
-    soup = BeautifulSoup(page, 'html.parser')
-    data = soup.find('div',class_='BNeawe iBp4i AP7Wnd')
-    print(data.text)
+# def stockPrice(query="Binance coin price"):
+#     URL = "https://www.google.com/search?q=" + query
+#     page = requests.get(URL).text
+#     soup = BeautifulSoup(page, 'html.parser')
+#     data = soup.find('div',class_='BNeawe iBp4i AP7Wnd')
+#     print(data.text)
 
-stockPrice()
+# stockPrice()
+
+import yfinance
 
