dd627d09de250703e41f16619c3cc8b72b7a1b42
BAPPAYNE <105190658+BAPPAYNE@users.noreply.github.com>
2025-06-06 12:44:15 +0000

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -4,8 +4,5 @@ pip.main(['install','pyttsx3'])
 pip.main(['install','pywhatkit'])
 pip.main(['install','wikipedia'])
 pip.main(['install','PyAudio'])
-<<<<<<< HEAD
 pip.main(['install','selenium'])
-=======
 pip.main(['install','selenium'])
->>>>>>> a8490130da6143f98e5cb5b7fdd5fc4650f4cb82
